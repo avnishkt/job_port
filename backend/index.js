8a5bd1ba363a8a4a11e1e3199af95b661a1690b3
avnish <avnishkt16@gmail.com>
2024-12-06 22:30:28 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -17,14 +17,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const allowedOrigins = ['http://localhost:5173', 'https://job-port-sage.vercel.app'];
-
 const corsOptions = {
-  origin: '*',
+  origin: function (origin, callback) {
+    const allowedOrigins = ['https://job-port-sage.vercel.app', 'https://job-portal-j3.vercel.app'];
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies and credentials
 };
+
 app.use(cors(corsOptions));
 
 
