a27ab5367758298a7a1a8aa53c176f8ae91e0aed
avnish <avnishkt16@gmail.com>
2024-12-06 22:19:51 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const allowedOrigins = ['http://localhost:5173', 'https://job-port-sage.vercel.app'];
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies and credentials
